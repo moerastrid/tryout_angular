e14ff869723f11748d3b1b7f2392ffe9eb8026ff
Astrid Geels <moerastrid@gmail.com>
2024-03-25 20:02:02 +0100

    added user & made a button to click to 'log in'

--- src/main.ts
+++ src/main.ts
@@ -5,20 +5,32 @@ import { BanaanComponent } from './app/banaan/banaan.component';
 import { KiwiComponent } from './app/kiwi/kiwi.component';
 
 @Component({
+	selector: 'user',
+	standalone: true,
+	styles: `
+		h2 {
+			color : white;
+		}
+		body {
+			color : white;
+		}
+	`,
+	templateUrl: './user.html',
+})
+export class User {
+	name = '';
+	isLoggedin = false;
+	channelsJoined = [{id: 1, name: "first!"}, {id: 2, name: "our fun channel"}, {id: 9, name: "number nine"}, {id: 2, name: "id's don't need to be unique lol"}];
+	logInNow() {
+		this.isLoggedin = true;
+	};
+}
+
+@Component({
   selector: 'app-root',
   standalone: true,
-  imports: [BanaanComponent, KiwiComponent],
-  template: `
-    <h1>Hello from {{ name }}!</h1>
-    <a target="_blank" href="https://angular.dev/overview">
-      Learn more about Angular
-    </a>
-	<body>
-	<app-banaan></app-banaan>
-	<app-kiwi></app-kiwi>
-	<app-banaan></app-banaan>
-	</body>
-  `,
+  imports: [BanaanComponent, KiwiComponent, User],
+  templateUrl: './main.html',
 })
 export class App {
   name = 'Angular';
